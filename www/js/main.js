f075614181b514c0b2fc9c764936ab9f0ae030eb
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2020-01-08 19:33:40 -0600

    cambios en paquetes

--- www/js/main.js
+++ www/js/main.js
@@ -1,19 +1,9 @@
-var React, ReactDOM, ons, Ons, createReactClass, parse;
+var  ons, Ons, Handlebars;
 
-React = require('react');
-createReactClass = require('create-react-class');
-ReactDOM = require('react-dom');
 ons = require('onsenui');
-Ons = require('react-onsenui');
-parse = require('html-react-parser');
 Handlebars = require('handlebars');
 
 /* load */
-
-window.React = React;
-window.ReactDOM = ReactDOM;
 window.ons = ons;
 window.Ons = Ons;
-window.createClass = createReactClass;
-window.parse = parse;
 window.Handlebars = Handlebars;
